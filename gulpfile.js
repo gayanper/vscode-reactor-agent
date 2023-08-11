d5bad7c24fe5506a6453d78a18b52cd6843764ef
Gayan Perera <gayanper@gmail.com>
2023-08-11 21:36:36 +0200

    add maven wrapper

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const gulp = require('gulp');
 const exec = require('child_process');
 
 gulp.task('build-agent', function(done) {
-    exec.execSync('mvn clean package', { cwd: 'agent/vscode-reactor-agent', stdio: [0,1,2]});
+    exec.execSync('./mvnw clean package', { cwd: 'agent/vscode-reactor-agent', stdio: [0,1,2]});
     gulp.src('agent/vscode-reactor-agent/target/ragent.jar').pipe(gulp.dest('ajars/'));
     done();
 });
