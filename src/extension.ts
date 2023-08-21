2bdf639b64cd67c129cf817a307944efc62da037
Gayan Perera <gayanper@gmail.com>
2023-08-21 21:11:32 +0200

    Enable reactor debug for unit tests

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,12 @@ import { existsSync } from 'fs';
 import path = require('path');
 import * as vscode from 'vscode';
 
+const TEST_ARG = "enableTracebacks";
+
 export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.debug.registerDebugConfigurationProvider("java", {
 		resolveDebugConfiguration(folder, debugConfiguration, token) {
-			if (debugConfiguration.enableTracebacks) {
+			if (isEnabled(debugConfiguration)) {
 				const extensionPath = vscode.extensions.getExtension("org-gap.vscode-reactor-launcher")?.extensionPath;
 				if (extensionPath) {
 					const agentPath = path.join(extensionPath, "ajars/ragent.jar");
@@ -33,6 +35,28 @@ function updateExistingValue(debugConfiguration: vscode.DebugConfiguration, agen
 	} else {
 		debugConfiguration.vmArgs = (debugConfiguration.vmArgs as string).concat(' ').concat(agentParam);
 	}
+}
+
+function isEnabled(debugConfiguration: vscode.DebugConfiguration): boolean {
+	if (debugConfiguration.enableTracebacks) {
+		return true;
+	} else if (debugConfiguration.vmArgs) {
+		if (Array.isArray(debugConfiguration.vmArgs)) {
+			const result = (debugConfiguration.vmArgs as Array<string>).includes(TEST_ARG);
+			if (result) {
+				const argArray = debugConfiguration.vmArgs as Array<string>;
+				argArray.splice(argArray.indexOf(TEST_ARG), 1);
+			}
+			return result;
+		} else {
+			const result = (debugConfiguration.vmArgs as string).includes(TEST_ARG);
+			if (result) {
+				debugConfiguration.vmArgs = (debugConfiguration.vmArgs as string).replace(" ".concat(TEST_ARG), "");
+			}
+			return result;
+		}
+	}
+	return false;
 }
 
 export function deactivate() { }
