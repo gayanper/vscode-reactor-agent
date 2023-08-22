acac835e2057d7e3d048bad8e12c74e8663bdcf4
Gayan Perera <gayanper@gmail.com>
2023-08-22 21:40:25 +0200

    major: change how configuration is done for enable tracebacks

--- src/extension.ts
+++ src/extension.ts
@@ -1,62 +1,61 @@
 import { existsSync } from 'fs';
 import path = require('path');
 import * as vscode from 'vscode';
-
-const TEST_ARG = "enableTracebacks";
+import { getLocation } from 'jsonc-parser';
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.debug.registerDebugConfigurationProvider("java", {
-		resolveDebugConfiguration(folder, debugConfiguration, token) {
-			if (isEnabled(debugConfiguration)) {
-				const extensionPath = vscode.extensions.getExtension("org-gap.vscode-reactor-launcher")?.extensionPath;
-				if (extensionPath) {
-					const agentPath = path.join(extensionPath, "ajars/ragent.jar");
-					if (existsSync(agentPath)) {
-						const agentParam = `-javaagent:${agentPath}`;
-						if (debugConfiguration.vmArgs) {
-							updateExistingValue(debugConfiguration, agentParam);
-						} else {
-							debugConfiguration.vmArgs = [agentParam];
-						}
-					} else {
-						vscode.window.showErrorMessage(`[Reactor Agent] agent jar not found at path ${agentPath}`);
+	context.subscriptions.push(registerTracebackCommand());
+	context.subscriptions.push(registerOptionCompletionProvider());
+}
+
+function registerOptionCompletionProvider() {
+	return vscode.languages.registerCompletionItemProvider({ language: 'jsonc' }, {
+		provideCompletionItems(document, position, token, context) {
+			if (document.fileName.endsWith('.code-workspace') || document.fileName.match('settings.json')
+				|| document.fileName.match('launch.json')) {
+
+				const jsonLocation = getLocation(document.getText(), document.offsetAt(position));
+				if (jsonLocation.isAtPropertyKey) {
+					return [];
+				}
+
+				const path = jsonLocation.path;
+				// support both java and unit test configs
+				if (path && (path.at(-1) === 'vmArgs' || path.at(-2) === 'vmArgs') || path.at(-2) === 'vmargs') {
+					let range = document.getWordRangeAtPosition(position, /".*"/);
+					if (range) {
+						range = new vscode.Range(position, position);
+						return [{
+							label: 'EnableTracebacks',
+							detail: 'Enable reactor tracebacks',
+							command: 'command:reactorLauncher.tracebacks'
+						}].map(i => ({
+							label: i.label,
+							detail: i.detail,
+							range: range,
+							insertText: (`\${${i.command}}`)
+						}));
 					}
 				}
 			}
-			return debugConfiguration;
-		},
-	});
-	context.subscriptions.push(disposable);
-}
-
-function updateExistingValue(debugConfiguration: vscode.DebugConfiguration, agentParam: string) {
-	if (Array.isArray(debugConfiguration.vmArgs)) {
-		debugConfiguration.vmArgs.unshift([agentParam]);
-	} else {
-		debugConfiguration.vmArgs = (debugConfiguration.vmArgs as string).concat(' ').concat(agentParam);
-	}
-}
-
-function isEnabled(debugConfiguration: vscode.DebugConfiguration): boolean {
-	if (debugConfiguration.enableTracebacks) {
-		return true;
-	} else if (debugConfiguration.vmArgs) {
-		if (Array.isArray(debugConfiguration.vmArgs)) {
-			const result = (debugConfiguration.vmArgs as Array<string>).includes(TEST_ARG);
-			if (result) {
-				const argArray = debugConfiguration.vmArgs as Array<string>;
-				argArray.splice(argArray.indexOf(TEST_ARG), 1);
-			}
-			return result;
-		} else {
-			const result = (debugConfiguration.vmArgs as string).includes(TEST_ARG);
-			if (result) {
-				debugConfiguration.vmArgs = (debugConfiguration.vmArgs as string).replace(" ".concat(TEST_ARG), "");
-			}
-			return result;
+			return [];
 		}
-	}
-	return false;
+	});
+}
+
+function registerTracebackCommand() {
+	return vscode.commands.registerCommand('reactorLauncher.tracebacks', () => {
+		const extensionPath = vscode.extensions.getExtension("org-gap.vscode-reactor-launcher")?.extensionPath;
+		if (extensionPath) {
+			const agentPath = path.join(extensionPath, "ajars/ragent.jar");
+			if (existsSync(agentPath)) {
+				return `-javaagent:${agentPath}`;
+			} else {
+				vscode.window.showErrorMessage(`[Reactor Launcher] agent jar not found at path ${agentPath}`);
+			}
+		}
+		return '';
+	});
 }
 
 export function deactivate() { }
